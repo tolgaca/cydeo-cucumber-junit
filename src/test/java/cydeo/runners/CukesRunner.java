e5184fc5af4e6e4cc7a1c4bddd639d779a10e0b5
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-03-21 13:49:03 +0300

    Cucumber notes

---
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -0,0 +1,25 @@
+package cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+    },
+        features = "src/test/resources/features",
+        glue = "cydeo/step_definitions",
+        dryRun = false,
+        tags = "@login",
+        publish = false
+
+)
+public class CukesRunner {
+}
+
+
